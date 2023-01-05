3301d5e353916abbb287445edefd4440febc080a
ruchi06 <ruchi06@gmail.com>
2023-01-05 21:33:15 +0530

    Single Player At Position 0

---
+++ SnakeLadderProblem/SnakeLadderGames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class SnakeLadderGame
+    {
+        public void SnakeLadder()
+        {
+            int position1 = 0;
+            int Num_of_Players = 1;
+
+            Console.WriteLine("Enter s to Start to play ");
+            string start = Console.ReadLine();
+
+            Console.WriteLine("Number of Players : " + Num_of_Players);
+            Console.WriteLine("Player 1 at position : " + position1);
+
+        }
+    }
+}
