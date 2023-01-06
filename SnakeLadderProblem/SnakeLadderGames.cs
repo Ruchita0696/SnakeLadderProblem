b79d7fb5242d2e9e580cc26371153aabcbb6c8ff
ruchi06 <ruchi06@gmail.com>
2023-01-06 09:30:06 +0530

    Single Player position At 0

--- SnakeLadderProblem/SnakeLadderGames.cs
+++ SnakeLadderProblem/SnakeLadderGames.cs
@@ -10,63 +10,17 @@ namespace SnakeLadderProblem
     {
         public void SnakeLadder()
         {
-            int Position1 = 0;
-            int count1 = 0;
+            int position1 = 0;
             int Num_of_Players = 1;
-            
 
             Console.WriteLine("Enter s to Start to play ");
             string start = Console.ReadLine();
 
             Console.WriteLine("Number of Players : " + Num_of_Players);
-            Console.WriteLine("Player 1 at position : " + Position1);
-
-            Random random = new Random();
-            while (Position1 != 100)
-            {
-                int DiceNumber1 = random.Next(1, 7);
-               Console.WriteLine("Player rolled the Die ....");
-               Console.WriteLine("Dies turns to : " + DiceNumber1);
-
-               int Option1 = random.Next(1, 4);
-
-                switch (Option1)
-                {
-                    case 1:
-                        Console.WriteLine("you are at No Play option");
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
-
-                    case 2:
-                        Console.WriteLine("you Get Move Ahead option");
-                        Console.WriteLine("Great! You get to Move Ahead, By " + DiceNumber1);
-                        Position1 = Position1 + DiceNumber1;
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
-
-                    case 3:
-                        Console.WriteLine("But you get Move back option");
-                        Console.WriteLine("Oops! you Need to Move Back, By" + DiceNumber1);
-                        Position1 = Position1 - DiceNumber1;
-                        Console.WriteLine("Current Position: " + Position1);
-                        break;
-                }
-                if (Position1 < 0)
-                {
-                    Console.WriteLine("Please Restart");
-                    Position1 = 0;
-                    continue;
-                }
-                if (Position1 == 100)
-                {
-                    Console.WriteLine("Player One wins");
-                    break;
-                }
-                count1++;
-            }
-            Console.WriteLine("Total Number of Times Dies has turn to win is " + count1);
+            Console.WriteLine("Player 1 at position : " + position1);
 
         }
     }
-    
 }
+    
+
